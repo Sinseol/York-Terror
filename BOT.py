1a882d71a0b81ee73e71f2c6455deb3c955e493d
Sinseol <82746506+Sinseol@users.noreply.github.com>
2021-04-18 23:29:43 +0900

    Bot.py

--- BOT.py
+++ BOT.py
@@ -3,27 +3,25 @@ import asyncio
 
 client = discord.Client()
 
-token = "ODE2Njc1MTA1MTgyMDU2NTQ4.YD-Z9g.REwDmLgQYsgUO17OCaX-qm4CHsw"
+token = "봇토큰"
 
 @client.event
 async def on_ready():
 
     print(client.user.name)
     print('봇이 시작되었습니다')
-    game = discord.Game('York Terror Server')
+    game = discord.Game('봇상메')
     await client.change_presence(status=discord.Status.online, activity=game)
 
 @client.event
 async def on_message(message):
-    if message.content == "s!help":
-        await message.channel.send("""```명령어 목록입니다.
-1.s!디스코드
-2.s!ping```""")
+    if message.content == "감지할 말":
+        await message.channel.send("출력할말")
 
-    if message.content == "s!디스코드":
-        await message.channel.send("https://discord.gg/Q3vxAFBbTG")
+    if message.content == "감지할 말":
+        await message.channel.send("출력할말")
 
-    if message.content == "s!ping":
-        await message.channel.send("pong!")
+    if message.content == "감지할 말":
+        await message.channel.send("출력할말")
 
 client.run(token)
